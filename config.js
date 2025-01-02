c5e656c062440a06d567f9510c2665f5a0983dcb
lyfe05 <mastercarder.169@gmail.com>
2025-01-03 00:32:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-	SESSION_ID: process.env.SESSION_ID || '',
-	SUDO: process.env.SUDO || '',
+	SESSION_ID: process.env.SESSION_ID || 'XSTRO_12_13_30',
+	SUDO: process.env.SUDO || '254105008363',
 	API_ID: process.env.API_ID || 'https://xstro-api-4fb28ece11a9.herokuapp.com',
 	BOT_INFO: process.env.BOT_INFO || 'Astro;Xstro_Bot',
 	STICKER_PACK: process.env.STICKER_PACK || 'Xstro;Md',
 	WARN_COUNT: process.env.WARN_COUNT || 3,
-	TIME_ZONE: process.env.TIME_ZONE || 'Africa/Lagos',
+	TIME_ZONE: process.env.TIME_ZONE || 'Africa/Nairobi',
 	DEBUG: process.env.DEBUG || false,
 	VERSION: '1.2.0',
 };
